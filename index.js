1ed7694579e65db210408ec075056a931a9c1be5
Thomas <lefoux82@gmail.com>
2021-06-24 16:20:02 +0200

    server is ok

--- index.js
+++ index.js
@@ -2,22 +2,26 @@ const express = require("express");
 var http = require("http");
 const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 20;
 var server = http.createServer(app);
 var io = require("socket.io")(server);
 
 //middlewre
 app.use(express.json());
-var clients = {};
 
-io.on("connection", (socket) => {
+io.on("Connection", (socket) => {
   console.log("connetetd");
   console.log(socket.id, "has joined");
-  socket.on("signin", (id) => {
+  socket.on('/test', (msg)=>{
+    console.log(msg);
+  }
+
+  );
+  "signin", (id) => {
     console.log(id);
     clients[id] = socket;
     console.log(clients);
-  });
+  }
   socket.on("message", (msg) => {
     console.log(msg);
     let targetId = msg.targetId;
@@ -27,4 +31,5 @@ io.on("connection", (socket) => {
 
 server.listen(port, "0.0.0.0", () => {
   console.log("server started");
+  console.log(port);
 });
